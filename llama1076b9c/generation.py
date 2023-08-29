4028d2baa9931b4c8768ada3e805a2c8e400aa70
jason-furiosa <jason@furiosa.ai>
2023-08-29 04:27:31 +0000

    fix recompilation in decoding

--- llama1076b9c/generation.py
+++ llama1076b9c/generation.py
@@ -28,25 +28,34 @@ class LLaMA:
             torch._dynamo.reset()
             self._generate_one_token_fn = torch.compile(self._generate_one_token_fn, backend=custom_backend)
 
-    def _generate_one_token(self, tokens, input_text_mask,
-                            cur_pos, prev_pos, cache_kvs,
-                            temperature,
-                            top_p):
-        input_pos_tensor = torch.arange(prev_pos, cur_pos).cuda()
-        output_pos_tensor = torch.tensor(cur_pos - 1).cuda()
-        logits, cache_kvs = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos, input_pos_tensor, output_pos_tensor, cache_kvs)
-        if temperature > 0:
-            probs = torch.softmax(logits / temperature, dim=-1)
-            next_token = sample_top_p(probs, top_p)
+    def _generate_one_token(self, tokens, input_tokens, prev_pos, input_text_mask,
+                            cur_pos_tensor, input_pos_tensor,
+                            output_pos_tensor, cache_kvs, temperature_tensor,
+                            top_p_tensor, with_temp):
+        logits, cache_kvs = self.model(input_tokens, prev_pos, input_pos_tensor, output_pos_tensor, cache_kvs)
+        if with_temp:
+            probs = torch.softmax(logits / temperature_tensor, dim=-1)
+            next_token = sample_top_p(probs, top_p_tensor)
         else:
             next_token = torch.argmax(logits, dim=-1)
         next_token = next_token.reshape(-1)
         # only replace token if prompt has already been generated
+        input_text_mask_tmp = input_text_mask.index_select(1, cur_pos_tensor).squeeze(dim=1)
+        tokens_tmp = tokens.index_select(1, cur_pos_tensor).squeeze(dim=1)
         next_token = torch.where(
-            input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
+            input_text_mask_tmp, tokens_tmp, next_token
         )
-        tokens[:, cur_pos] = next_token
-        return tokens, cache_kvs
+        # tokens[:, cur_pos] = next_token
+        next_token = next_token.unsqueeze(1)
+        tokens.index_copy_(1, cur_pos_tensor, next_token)
+        # prepare for the next iteration
+        input_pos_tensor = cur_pos_tensor.unsqueeze(0)
+        cur_pos_tensor = cur_pos_tensor + 1
+        output_pos_tensor = cur_pos_tensor - 1
+        input_tokens = tokens.index_select(1, input_pos_tensor)
+
+        return tokens, input_tokens, cur_pos_tensor, input_pos_tensor, output_pos_tensor, cache_kvs
+
 
     def generate(
         self,
@@ -55,6 +64,7 @@ class LLaMA:
         temperature: float = 0.8,
         top_p: float = 0.95,
     ) -> List[str]:
+        device = 'cuda'
         bsz = len(prompts)
         params = self.model.params
         assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
@@ -66,11 +76,19 @@ class LLaMA:
 
         total_len = min(params.max_seq_len, max_gen_len + max_prompt_size)
 
-        tokens = torch.full((bsz, total_len), self.tokenizer.pad_id).cuda().long()
+        tokens = torch.full((params.max_batch_size, params.max_seq_len), self.tokenizer.pad_id).cuda().long()
         for k, t in enumerate(prompt_tokens):
             tokens[k, : len(t)] = torch.tensor(t).long()
+        tokens = tokens.to(device)
+
         input_text_mask = tokens != self.tokenizer.pad_id
         start_pos = min_prompt_size
+
+        # Passing tensors instead of floats into self._generate_one_token_fn,
+        # so that different values would not trigger compilations of new graphs
+        temperature_tensor = torch.tensor(float(temperature)).to(device)
+        top_p_tensor = torch.tensor(float(top_p)).to(device)
+        with_temp = temperature > 0
 
         cache_kvs = self.model.cache_kvs
 
@@ -84,8 +102,18 @@ class LLaMA:
             if i == 1:
                 print(f"{i}-th pre-fill-phase")
             else:
-                print(f"{i}-th decoding phase")
-            tokens, cache_kvs = self._generate_one_token_fn(tokens, input_text_mask, cur_pos, prev_pos, cache_kvs, temperature, top_p)
+                print(f"{i-1}-th decoding phase")
+            prev_pos_tensor = torch.tensor(prev_pos).to(device)
+            cur_pos_tensor = torch.tensor(cur_pos).to(device)
+            output_pos_tensor = torch.tensor(cur_pos - 1).to(device)
+            input_pos_tensor = torch.arange(prev_pos, cur_pos).to(device)
+            input_tokens = tokens.index_select(1, input_pos_tensor)
+            tokens, input_tokens, cur_pos_tensor, input_pos_tensor, output_pos_tensor, cache_kvs \
+                = self._generate_one_token_fn(
+                    tokens, input_tokens, prev_pos_tensor, input_text_mask, cur_pos_tensor,
+                    input_pos_tensor, output_pos_tensor, cache_kvs,
+                    temperature_tensor, top_p_tensor, with_temp
+                )
             prev_pos = cur_pos
         self.model.cache_kvs = cache_kvs
 
